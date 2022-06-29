f41791b5ac9daf4897bc70cd30d4b741ee1aa498
amyfranses <amyfranses@gmail.com>
2022-06-29 18:25:34 +0000

    Pair programming code for math game

---
+++ Main.rb
@@ -0,0 +1,66 @@
+require './player.rb'
+require './questions.rb'
+
+
+class Game
+
+  def initialize
+    @current_player = 'Amy'
+
+    @player1 = Player.new('Amy')
+    @player2 = Player.new('Manuel')
+
+    @player1_lives = "#{@player1.lives}/3"
+    @player2_lives = "#{@player2.lives}/3"
+
+    @end = false
+
+    until @end
+      question_loop
+    end
+
+  end
+
+  def question_loop
+    puts '--- NEW TURN ---'
+    ask_question = Questions.new(@current_player)
+    #If incorrect, 1 life less
+    if !ask_question.correct
+    incorrect
+    end
+   #Change player
+    if @current_player == 'Amy'
+      @current_player = 'Manuel'
+
+    else @current_player == 'Manuel'
+     @current_player = 'Amy'
+    end
+
+    puts "P1: #{@player1.name} #{@player1_lives} vs P2: #{@player2.name} #{@player2_lives}"
+  end
+
+  def incorrect
+    #Check it the player still has lifes
+    if @current_player == 'Amy'
+      @player1.incorrect
+      @player1_lives = "#{@player1.lives}/3"
+
+      if @player1.lives === 0
+        @end = true
+        puts '--- GAME OVER, PAL ---'
+  end
+
+    else
+      @player2.incorrect
+      @player2_lives = "#{@player2.lives}/3"
+
+      if @player2.lives === 0
+        @end = true
+        puts '--- GAME OVER, PAL ---'
+
+      end
+    end
+  end
+end
+
+start = Game.new
---
+++ Question.rb
@@ -0,0 +1,26 @@
+class Question
+  attr_accessor :correct
+
+  def initialize(current_player)
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = @num1 + @num2
+    @current_player = current_player
+
+    puts "#{current_player}: What's #{@num1} plus #{@num2} ?"
+    ask
+  end
+
+  def ask
+    @player_answer = gets.chomp
+   
+    if @player_answer == @answer.to_s
+      puts "#{@current_player}, well done, that's the correct answer"
+      @correct = true
+    
+    else
+      puts "#{@current_player}, Incorrect answer"
+      @correct = false
+    end
+  end
+end
